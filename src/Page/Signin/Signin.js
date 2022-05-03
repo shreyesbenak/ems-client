1a7821dd98babe1d81c266bd38aa8c07b1f0b06b
ArnabC49 <46952316+codeR49@users.noreply.github.com>
2022-05-03 16:05:24 +0530

    login api

--- src/Page/Signin/Signin.js
+++ src/Page/Signin/Signin.js
@@ -1,8 +1,7 @@
-import React, {useState} from 'react'
-import { Switch, Redirect,Route } from 'react-router-dom';
+import React, { useState } from 'react'
+import { Switch, Redirect, Route } from 'react-router-dom';
 import { Grid, Paper, Avatar, TextField, Button, Typography, Link } from '@material-ui/core'
-
-// import DevelopmentUrl from '../../data/api';
+import DevelopmentUrl from '../../data/api';
 import Axios from 'axios';
 import jwt_decode from "jwt-decode";
 
@@ -12,9 +11,9 @@ import alchemylogo from "../../image/638-01.png"
 
 const Signin = () => {
 
-    const paperStyle = { padding: 20, height: '60vh', width: 350, margin: "100px auto"  }
+    const paperStyle = { padding: 20, height: '60vh', width: 350, margin: "100px auto" }
     const avatarStyle = { backgroundColor: '#1bbd7e' }
-    const btnstyle = { margin: '30px 0'   }
+    const btnstyle = { margin: '30px 0' }
     const txtstyle = { margin: "10px 0" }
     const signintxtStyle = { marginTop: "40px" }
     let [username, setUsername] = useState('');
@@ -36,48 +35,48 @@ const Signin = () => {
             username: username,
             password: password
         };
-        // Axios.post(DevelopmentUrl + '/users/login', formdata).then(
-        //     (res) => {
-        //         let { token } = res.data;
-        //         //console.log(token);
-        //         localStorage.setItem('token', token);
-        //         setloginstatus(res.status);
+        Axios.post(DevelopmentUrl + '/users/login', formdata).then(
+            (res) => {
+                let { token } = res.data;
+                //console.log(token);
+                localStorage.setItem('token', token);
+                setloginstatus(res.status);
 
-        //     }
-        // ).catch(error => {
-        //     console.log("error occured")
-        //     console.log(error.data)
-        // })
+            }
+        ).catch(error => {
+            console.log("error occured")
+            console.log(error.data)
+        })
     }
     if (loginstatus === 200) {
         let isAdmin = jwt_decode(localStorage.getItem("token")).admin;
         localStorage.setItem("admin", isAdmin);
-       
-        message = 
-        <Switch>
-            {!isAdmin ? <Redirect to='/home'/>:<Redirect to='/admin/dashboard'/>}
-            
-       </Switch>
+
+        message =
+            <Switch>
+                {!isAdmin ? <Redirect to='/home' /> : <Redirect to='/admin/dashboard' />}
+
+            </Switch>
 
         return message;
 
     }
     return (
         <div >
-            
 
-        <div className='login'>
-        <form onSubmit={submithandler}>
+
+            <div className='login'>
+                <form onSubmit={submithandler}>
                     <Grid>
-                    <Paper elevation={10} style={paperStyle}>
-                        <Grid align='center'>
-                        <img src={alchemylogo} height="60px"/>
-                            {/* <h2>Sign In</h2> */}
-                        </Grid>
-                        <TextField label='Username' placeholder='Enter username' type='text' id ='username' fullWidth required style={signintxtStyle} onChange={onchangeusernamehandler}/>
-                        <TextField label='Password' placeholder='Enter password' type='password' id='password' fullWidth required style={txtstyle} onChange={onchangepasswordhandler}/>
-        
-                        {/* <FormControlLabel style={txtstyle}
+                        <Paper elevation={10} style={paperStyle}>
+                            <Grid align='center'>
+                                <img src={alchemylogo} height="60px" />
+                                {/* <h2>Sign In</h2> */}
+                            </Grid>
+                            <TextField label='Username' placeholder='Enter username' type='text' id='username' fullWidth required style={signintxtStyle} onChange={onchangeusernamehandler} />
+                            <TextField label='Password' placeholder='Enter password' type='password' id='password' fullWidth required style={txtstyle} onChange={onchangepasswordhandler} />
+
+                            {/* <FormControlLabel style={txtstyle}
                             control={
                             <Checkbox
                                 name="checkedB"
@@ -86,23 +85,23 @@ const Signin = () => {
                             }
                             label="Remember me"
                          /> */}
-                        <Button type='submit'  color='primary' variant="contained" style={btnstyle } fullWidth>Sign in</Button>
-                        <Typography >
-                            <Link href="#" >
-                                Forgot password ?
-                            </Link>
-                        </Typography>
-                        {/* <Typography > Not registered?
+                            <Button type='submit' color='primary' variant="contained" style={btnstyle} fullWidth>Sign in</Button>
+                            <Typography >
+                                <Link href="#" >
+                                    Forgot password ?
+                                </Link>
+                            </Typography>
+                            {/* <Typography > Not registered?
                             <Link href="/signup" >
                                 Create account
                             </Link>
                         </Typography> */}
-                    </Paper>
-                </Grid>
+                        </Paper>
+                    </Grid>
                 </form>
+            </div>
         </div>
-                    </div>
-        
+
     )
 }
 
---
+++ src/data/api.js
@@ -0,0 +1,2 @@
+const DevelopmentUrl = "http://127.0.0.1:7000";
+module.exports =  DevelopmentUrl;
